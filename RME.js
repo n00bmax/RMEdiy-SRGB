f55d093ddd2be71e6bc79debc25277007cb290a7
Udit Vasudevan <udit@kvudit.in>
2025-08-09 21:46:46 -0700

    test

--- RME.js
+++ RME.js
@@ -6,72 +6,197 @@ export function Size() { return [20, 20]; }
 export function DefaultPosition() {return [75, 70]; }
 export function DefaultScale(){return 1.0;}
 
+export function ControllableParameters(){
+  return [
+    {"property":"shutdownColor", "group":"lighting", "label":"Shutdown Color", description: "This color is applied to the device when the System, or SignalRGB is shutting down", "min":"0", "max":"360", "type":"color", "default":"#000000"},
+		{"property":"LightingMode", "group":"lighting", "label":"Lighting Mode", description: "Determines where the device's RGB comes from. Canvas will pull from the active Effect, while Forced will override it to a specific color", "type":"combobox", "values":["Canvas", "Forced"], "default":"Canvas"},
+		{"property":"forcedColor", "group":"lighting", "label":"Forced Color", description: "The color used when 'Forced' Lighting Mode is enabled", "min":"0", "max":"360", "type":"color", "default":"#009bde"},   
+  ];
+}
+/* global
+controller:readonly
+*/
 
+let streamingAddress = "";
+let streamingPort = "";
+
+export function SubdeviceController(){ return true; }
+
+
+export function Initialize() {
+	device.setName(controller.name);
+
+	device.addFeature("udp");
+
+	streamingAddress = "127.0.0.1";
+	streamingPort = 61447;
+
+  spawnSubdevices();
+
+}
+
+export function Render() 
+{
+  if(controller.children.primary !== undefined) {
+    grabColors(controller.children.primary);
+  }  
+}
+
+export function Shutdown() {
+
+}
+
+function spawnSubdevices() {
+  if(controller.children.primary !== undefined) {
+    spawnSubdevice(controller.children.primary);
+  } 
+}
+
+function spawnSubdevice(subdevice) {
+  device.createSubdevice(subdevice.name);
+
+	device.setSubdeviceName(subdevice.name, `${"LianLi Colordrop"} - ${subdevice.name}`);
+
+	device.setSubdeviceLeds(subdevice.name, subdevice.ledNames, subdevice.ledPositions);
+
+	device.setSubdeviceSize(subdevice.name, subdevice.size[0], subdevice.size[1]);
+
+  device.setSubdeviceImageUrl(subdevice.name, subdevice.image);
+}
+
+function grabColors(subdevice, overrideColor) {
+	const vLedPositions = subdevice.ledPositions;
+  const rgbData = [];
+
+  for(let leds = 0; leds < 400; leds++) {
+    let col;
+
+    if(overrideColor) {
+      col = hexToRgb(overrideColor);
+    } else if (LightingMode === "Forced") {
+      col = hexToRgb(forcedColor);
+    } else {
+      col = device.subdeviceColor(subdevice.name, vLedPositions[leds][0], vLedPositions[leds][1]);
+    }
+
+    rgbData[leds * 3] = col[0];
+    rgbData[leds * 3 + 1] = col[1];
+    rgbData[leds * 3 + 2] = col[2];
+  }
+
+  sendZonePacket(subdevice.offset, rgbData);
+}
+
+function sendZonePacket(zone, rgbData) {
+  udp.send(streamingAddress, streamingPort, [    
+    0x1e, //led count
+  ].concat(rgbData));
+}
+
+// -------------------------------------------<( Discovery Service )>--------------------------------------------------
+
+export function DiscoveryService() {
+	this.IconUrl = "https://assets.signalrgb.com/brands/custom/lianli/logo.png";
+
+  // This will eventually poll to get a list of available devices.
+  // 127.0.0.1:61337
+
+	this.timeSinceLastReq = 0;
+	this.Retries = 5;
+	this.RetryCount = 0;
+  this.initialized = false;
+
+	this.Initialize = function() {
+    
+	};
+
+
+	this.Update = function() {
+    // Autodiscover.
+    if (!this.initialized){
+      this.Discovered();
+      this.initialized = true;
+    }    
+	};
+
+	this.Discovered = function() {
+		this.CreateControllerDevice("20000"); //placeholder ID
+	};
+
+
+  this.CreateControllerDevice = function(value){
+		const controller = service.getController(value);
+
+		if (controller === undefined) {
+      const cont = new LianLiParent(value)
+
+			service.addController(cont);
+
+      service.announceController(cont);
+		} else {
+			controller.updateWithValue(value);
+		}
+	};
+}
+
+class LianLiParent {
+  constructor(id){
+		this.updateWithValue(id);
+    this.children = {};
+
+    this.populateChildren();
+	}
+
+  populateChildren() {    
+    this.children.primary = new LianLiSwatch();    
+  }
+
+	updateWithValue(id){
+		this.id = id;
+		this.port = 61447;
+		this.ip = "127.0.0.1";
+    this.name = "LianLi Colordrop Service";
+
+		service.updateController(this);
+	}
+}
+
+class LianLiSwatch {
+	constructor(value) 
+  {
+    this.offset = value;
+    this.typename = this.offsetToName(this.offset);    
+    this.image = this.offsetToImage(this.offset);
+		this.name = this.typename; //calc zone here based on offset
+    this.generateLeds(400);
+	}
+
+  generateLeds(count) {
+    const vLedNames = [];
+    const vLedPositions = [];
+  
+    for(let iIdx = 0; iIdx < count; iIdx++) {
+      var x = iIdx % 20;
+      var y = iIdx / 20;            
+      vLedNames.push(`LED ${iIdx + 1}`);
+      vLedPositions.push([x, y]);
+    }
+  
+    this.ledNames = vLedNames;
+    this.ledPositions = vLedPositions;
+    this.size = [20,20];
+  }
+
+  offsetToName(offset) {
+    return "Primary";
+  }
+
+  offsetToImage(offset) {    
+      return "https://assets.signalrgb.com/devices/brands/lian-li/fan-controllers/strimmer-controller.png";    
+  }
+}
 
 export function ImageUrl()
 {
   return "https://assets.signalrgb.com/partners/lian-li/logo.png";
-}
-
-
-
-export function ControllableParameters() {
-  return [
-    {
-      property: "colorInput",
-      label: "Select Color",
-      type: "color",
-      default: "#FF0000",
-      group: "settings",
-    },
-  ];
-}
-
-let lastColor = null;
-
-export function Initialize() {
-  service.log("HTTP Color Sender Addon initialized");
-  lastColor = controller.colorInput;
-  sendColorRequest(lastColor);
-}
-
-export function oncolorInputChanged() {
-  if (controller.colorInput !== lastColor) {
-    lastColor = controller.colorInput;
-    service.log(`Color changed to ${lastColor}, sending HTTP request...`);
-    sendColorRequest(lastColor);
-  }
-}
-
-function sendColorRequest(colorHex) {
-  // Change this URL to your target endpoint
-  const url = `http://localhost:3333/metercolor/1`;
-
-  XmlHttp.Get(url, (xhr) => {
-    if (xhr.readyState === 4) {
-      if (xhr.status >= 200 && xhr.status < 300) {
-        service.log(`HTTP request successful: ${url}`);
-      } else {
-        service.log(`HTTP request failed: ${xhr.status} ${xhr.statusText}`);
-      }
-    }
-  });
-}
-
-// Simple XMLHttpRequest helper
-class XmlHttp {
-  static Get(url, callback) {
-    const xhr = new XMLHttpRequest();
-    xhr.open("GET", url, true);
-    xhr.onreadystatechange = () => callback(xhr);
-    xhr.send();
-  }
-}
-
-export function Shutdown() {
-  service.log("HTTP Color Sender Addon shutting down");
-}
-
-export function ImageUrl() {
-  return "https://assets.signalrgb.com/brands/srgbmods/logo.jpg";
 }
--- RME.qml
+++ RME.qml
@@ -1,70 +1,63 @@
-import QtQuick 2.15
-import QtQuick.Layouts 1.15
-import QtQuick.Controls 2.15
+import QtQuick.Layouts
 
 Item {
-    anchors.fill: parent
+  anchors.fill: parent
+  Column{
+    width: parent.width
+    height: parent.height
 
-    Column {
-        anchors.fill: parent
-        anchors.margins: 20
-        spacing: 15
+    Text{
+      color: "#FFF"
+      text: "Colordrop - Canvas Forward"
+      font.pixelSize: 20
+      font.family: "Poppins"
+      font.bold: true
+    },
 
-        Text {
-            color: "#FFFFFF"
-            text: "Colordrop - Canvas Forward"
+    Text{
+      color: "#FFF"
+      text: "This addon is used to send color information to lianli software, so that it can be used to create looping animations for lianli's wireless fan series.  A 20x20 pixel matrix is sent to 127.0.0.1:61447."
+      font.pixelSize: 12
+      font.family: "Montserrat"      
+    },
+
+    Repeater {
+      model: service.controllers          
+      delegate: Item {
+        width: 300
+        height: 250
+        Rectangle {
+          width: parent.width
+          height: parent.height - 10
+          color: "#FFFFFF"
+          radius: 5
+        }
+        Image {
+          id: logo
+          x: 10
+          y: 10
+          height: 80              
+          source: "https://assets.signalrgb.com/partners/lian-li/logo.png"
+          fillMode: Image.PreserveAspectFit
+          antialiasing: true
+          mipmap:true
+        }
+        Column {
+          x: 10
+          y: 100
+          width: parent.width - 20
+          spacing: 10
+          
+          Text{
+            color: "#000000"
+            text: model.modelData.obj.name
             font.pixelSize: 20
             font.family: "Poppins"
             font.bold: true
+          }               
         }
-
-        Text {
-            color: "#FFFFFF"
-            text: "This addon is used to send color information to LianLi software, so that it can be used to create looping animations for LianLi's wireless fan series. A 20x20 pixel matrix is sent to 127.0.0.1:61447."
-            font.pixelSize: 12
-            font.family: "Montserrat"
-            wrapMode: Text.WordWrap
-        }
-
-        Repeater {
-            model: service.controllers
-
-            delegate: Item {
-                width: 300
-                height: 250
-
-                Rectangle {
-                    anchors.fill: parent
-                    color: "#FFFFFF"
-                    radius: 5
-                }
-
-                Image {
-                    id: logo
-                    x: 10
-                    y: 10
-                    height: 80
-                    source: "https://assets.signalrgb.com/partners/lian-li/logo.png"
-                    fillMode: Image.PreserveAspectFit
-                    antialiasing: true
-                    mipmap: true
-                }
-
-                Column {
-                    x: 10
-                    y: 100
-                    width: parent.width - 20
-                    spacing: 10
-
-                    Text {
-                        color: "#000000"
-                        text: model.modelData.obj.name
-                        font.pixelSize: 20
-                        font.family: "Poppins"
-                        font.bold: true
-                    }
-                }
-            }
-        }
+        
+      }  
     }
+  }
 }
